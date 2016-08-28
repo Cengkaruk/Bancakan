9f020c8dcc47b6dfc17f709aee2a08ab99345a8e
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-08-28 21:55:52 +0700

    Fix bug https://github.com/BancakanID/Bancakan/issues/4

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof ModelNotFoundException) {
+            return session('_previous') ?  back() : redirect('/');
+        }
+
         return parent::render($request, $e);
     }
 }
