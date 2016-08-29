0af5890706e2b98704a3aca3b114cfc7aa7dba96
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-08-30 01:59:01 +0700

    Answer is belongs to Question

--- app/Answer.php
+++ app/Answer.php
@@ -16,6 +16,11 @@ class Answer extends Model
     return $this->belongsTo('App\User');
   }
 
+  public function question()
+  {
+    return $this->belongsTo('App\Question');
+  }
+
   public function votes()
   {
     return $this->hasMany('App\VoteAnswer');
