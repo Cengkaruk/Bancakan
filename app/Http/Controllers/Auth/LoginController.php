e0cea64ec3fb2f8e9bccad85684ac2e1881f3727
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-08-29 17:46:39 +0700

    New login implementation

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only(['email', 'password']), $request->remember)) {
+            return redirect()->intended('/');
+        }
+
+        return back()->withInput()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
